a8a2f17541d3d90bfa4c5ac76deea874d7412be3
Devika-0-0-3 <mailtodevika2003@gmail.com>
2025-06-09 23:01:09 +0530

    me

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import t from './assets/t.png';
 
 const App = () => {
   return (
-    <div className='container mx-auto'>
+    <div className='container '>
       
       <div className='upper'>
 
         <div className='first'>
-          <h1 className='head'>Transparent</h1>
-          <p className='tail'>STAY IN THE KNOW</p>
-          <div className='icon-group'>              
-            <img src={f} alt='icon f'/>
-            <img src={s} alt='icon s'/>
-            <img src={t} alt='icon t'/>
-          </div>
+            <h1 className='head'>Transparent</h1>
+            <p className='tail'>STAY IN THE KNOW</p>
+            <div className='flex  gap-1 w-6 h-6' >              
+              <img src={f} alt=''/>
+              <img src={s} alt=''/>
+              <img src={t} alt=''/>
+            </div>
         </div>
 
         <div className='second'>
@@ -45,16 +45,18 @@ const App = () => {
           </ul>
         </div>
 
-        <div className='fourth'>
+         <div className='fourth '>
           <h1 className='topic'>COMPANY</h1>
           <ul className='under'>
             <li>About us</li>
           </ul>
         </div>
-
+    
       </div>
 
-      <div className='stunderline lower'>
+      
+
+      <div className=' stunderline  lower'>
         <div className='fir'>
           <p>2024 Transparent. All Rights Reserved.</p>
           <ul className='list'>
@@ -63,13 +65,12 @@ const App = () => {
             <li>Technical Security</li>
           </ul>
         </div>
-        <p>
-          This Privacy Policy supplements our Terms of Service and any capitalized terms used, but not defined in this Privacy Policy, have the meaning given to them in our Terms of Service. Users can access the Services via our website at transparent.com. By using our Services, you are consenting to the collection, transfer, processing, storage, disclosure and other uses described in this privacy Policy.
-        </p>
+        <p>This Privacy Policy supplements our Terms of Service and any capitalized terms used, but not defined in this Privacy Policy, have the meaning given to them in our Terms of Service. Users can access the Services via our website at transparent.com. By using our Services, you are consenting to the collection, transfer, processing, storage, disclosure and other uses described in this privacy Policy.</p>
       </div>
+
 
     </div>
   )
 }
 
-export default App;
+export default App

0f89727b1289a0ca7dc75a2137ae1fe61b054ea3
Devika-0-0-3 <mailtodevika2003@gmail.com>
2025-06-09 22:57:57 +0530

    icons-not-aligned

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import f from './assets/f.png';
+import s from './assets/s.png';
+import t from './assets/t.png';
+
+const App = () => {
+  return (
+    <div className='container mx-auto'>
+      
+      <div className='upper'>
+
+        <div className='first'>
+          <h1 className='head'>Transparent</h1>
+          <p className='tail'>STAY IN THE KNOW</p>
+          <div className='icon-group'>              
+            <img src={f} alt='icon f'/>
+            <img src={s} alt='icon s'/>
+            <img src={t} alt='icon t'/>
+          </div>
+        </div>
+
+        <div className='second'>
+          <h1 className='topic'>PRODUCT</h1>
+          <ul className='under'>
+            <li>Live Conversation Platform</li>
+            <li>Dialer</li>
+            <li>Salesfloor</li>
+            <li>Analytics</li>
+            <li>Call Library</li>
+            <li>Pricing</li>
+          </ul>
+        </div>
+
+        <div className='third'>
+          <h1 className='topic'>SOLUTIONS</h1>
+          <ul className='under'>
+            <li>Build Pipeline</li>
+            <li>Increase Call Volume</li>
+            <li>Decrease Ramp-time</li>
+            <li>Coach</li>
+            <li>For Sales Reps</li>
+            <li>For Managers</li>
+            <li>For Sales Leaders</li>
+            <li>For Sales Operations</li>
+          </ul>
+        </div>
+
+        <div className='fourth'>
+          <h1 className='topic'>COMPANY</h1>
+          <ul className='under'>
+            <li>About us</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div className='stunderline lower'>
+        <div className='fir'>
+          <p>2024 Transparent. All Rights Reserved.</p>
+          <ul className='list'>
+            <li>Privacy policy</li>
+            <li>Terms of service</li>
+            <li>Technical Security</li>
+          </ul>
+        </div>
+        <p>
+          This Privacy Policy supplements our Terms of Service and any capitalized terms used, but not defined in this Privacy Policy, have the meaning given to them in our Terms of Service. Users can access the Services via our website at transparent.com. By using our Services, you are consenting to the collection, transfer, processing, storage, disclosure and other uses described in this privacy Policy.
+        </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
